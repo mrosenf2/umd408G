a41f9b6d1636d171ba05e8a5e412ee1c6077d4e2
ScottThomas07 <38081041+ScottThomas07@users.noreply.github.com>
2018-04-09 11:27:09 -0400

    Add files via upload

--- bestface.py
+++ bestface.py
@@ -5,52 +5,74 @@ import time
 import dlib
 import cv2
 
-def run(filepath):
-	detector = dlib.get_frontal_face_detector()
-	#predictor = dlib.shape_predictor(os.path.join(sys.path[0], "data/")) #ignore for now
 
-	#Start the frame queue
-	vs = FileVideoStream(filepath).start()
+class BF_object:
+    def __init__(self):
+    	self.maxframes = 0
+    	self.framecounter = 0
 
-	#Allow the videostream queue to populate
-	time.sleep(1)
+    def get_progress(self):
+        return self.framecounter
+    
+    def get_maxframes(self):
+    	return self.maxframes
 
-	framecounter = 0
-	best_frame = 0
-	scoremax = 0
+    def run(self, filepath):
+        detector = dlib.get_frontal_face_detector()
+        # predictor = dlib.shape_predictor(os.path.join(sys.path[0], "data/")) #ignore for now
 
-	# loop over the frames from the video stream
-	while vs.more():
+        # Start the frame queue
+        vs = FileVideoStream(filepath)
 
-	    # grab the frame from the threaded video stream, resize it to
-	    # have a maximum width of 400 pixels, and convert it to
-	    # grayscale
-	    frame = vs.read()
-	    frame = imutils.resize(frame, width=400)
-	    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	    # detect faces in the grayscale frame
-	    rects, scores, idx = detector.run(gray, 0, 0)
-	    # loop over the face detections
-	    for i, rect in enumerate(rects):
-            # compute the bounding box of the face
-	        (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
-	        # keep track of the highest scored face
-	        if (scores[i] > scoremax):
-	            scoremax = scores[i]
-	            best_face = frame[bY:bY + bH, bX:bX + bW]
-	            best_frame = framecounter
-	            #cv2.imshow("BEST_FACE", best_face)
+        if (not vs.stream.isOpened()):
+            return 0,0
 
-	    framecounter = framecounter + 1
+        self.maxframes = int(vs.stream.get(cv2.CAP_PROP_FRAME_COUNT))
+        
+        vs.start()
+        best_frame = 0
+        scoremax = 0
+
+        self.framecounter = 0
+        # loop over the frames from the video stream
+        while (self.framecounter < self.maxframes):
+
+            # grab the frame from the threaded video stream, resize it to
+            # have a maximum width of 400 pixels, and convert it to
+            # grayscale
+            frame = vs.read()
+            frame = imutils.resize(frame, width=400)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # detect faces in the grayscale frame
+            rects, scores, idx = detector.run(gray, 0, 0)
+            # loop over the face detections
+            self.framecounter = self.framecounter + 1
+
+            for i, rect in enumerate(rects):
+                # compute the bounding box of the face
+                (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
+                # keep track of the highest scored face
+                if (scores[i] > scoremax):
+                    scoremax = scores[i]
+                    best_face = frame[bY:bY + bH, bX:bX + bW]
+                    best_frame = self.framecounter
+                    #cv2.imshow("BEST_FACE", best_face)
+
+            
+
+            # if the q key was pressed, break from the loop
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+        # Stop the frame queue
+        vs.stop()
 
 
-	    # if the q key was pressed, break from the loop
-	    key = cv2.waitKey(1) & 0xFF
-	    if key == ord("q"):
-	        break
-	
-	#Stop the frame queue
-	vs.stop()
-	#Swap the R,B values
-	best_face = cv2.cvtColor(best_face, cv2.COLOR_BGR2RGB)
-	return best_face, best_frame
+        if (best_frame == 0):
+            #If no face was found
+            best_face = 0
+        else:
+            # Swap the R,B values
+            best_face = cv2.cvtColor(best_face, cv2.COLOR_BGR2RGB)
+        return best_face, best_frame
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import cv2
 import time
 import tkinter
-import bestface 
+import bestface
 from threading import Thread
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
@@ -19,15 +18,42 @@ def button_SelectFile():
 def button_Go():
 	def callback():
 		global found_face
-		found_face, framenum = bestface.run(txt_fn.get('1.0','end-1c'))
-		found_face = Image.fromarray(found_face)
-		found_face = ImageTk.PhotoImage(found_face)
-		canvas_bf.create_image(1,1, image= found_face,anchor='nw')
-		lbl_fc['text'] = 'The face was found on frame #' + str(framenum)
-		top.title("Team Shaspasms")	
+		found_face, framenum = test.run(filepath=txt_fn.get('1.0','end-1c'))
+		#If a face is found in the clip
+		if (framenum > 0):
+			#Create a photo object that can be placed into a canvas
+			found_face = Image.fromarray(found_face)
+			found_face = ImageTk.PhotoImage(found_face)
+			canvas_bf.create_image(100,100, image= found_face,anchor='center')
+			lbl_fc['text'] = 'The best face was found on frame #' + str(framenum)
+			top.title("Team Shaspasms")
+		#If a face wasn't found in the clip
+		else:
+			lbl_fc['text'] = 'NO FACES FOUND'
+		#Stop the thread
+		t.stopped = True
+	def update_progress():
+		def get_data():
+			cur_time = time.time()
+			cur_frame = test.get_progress()
+			lbl_pg['text']='Frame progress: ' + str(cur_frame) + '/' + str(max_frames) + '.     Elapsed time: ' + str(float("{0:.2f}".format(cur_time - start_time))) +'s'
+		start_time = time.time()
+		max_frames = 0
+		while max_frames == 0:
+			time.sleep(.25)
+			max_frames = test.get_maxframes()
+		while t.is_alive():
+			get_data()
+			time.sleep(1)
+		get_data()
+		r.stopped = True
 	top.title("WORKING...")
 	t = Thread(target=callback)
 	t.start()
+
+	r = Thread(target=update_progress)
+	r.start()
+		
 #widgets
 txt_fn = tkinter.Text(top, height=3, width=30)
 txt_fn.grid(row=0, column=0)
@@ -35,7 +61,7 @@ txt_fn.grid(row=0, column=0)
 btn_sf = tkinter.Button(top, text="Choose...",command=button_SelectFile)
 btn_sf.grid(row=0,column=1)
 
-canvas_bf = tkinter.Canvas(top, width = 300, height = 300)  
+canvas_bf = tkinter.Canvas(top, width = 200, height = 200)  
 canvas_bf.grid(row=1,column=0)
 
 btn_go = tkinter.Button(top, text="Go",command=button_Go)
@@ -43,6 +69,11 @@ btn_go.grid(row=0,column=2)
 
 lbl_fc = tkinter.Label(top)
 lbl_fc.grid(row=2,column=0)
+
+lbl_pg = tkinter.Label(top)
+lbl_pg.grid(row=3,column=0)
 ###############END GUI DEFINITIONS
+
+test = bestface.BF_object()
 
 top.mainloop()
