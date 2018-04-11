46e8c285db609d4c574c08e3377a0c1b273fdcbf
ScottThomas07 <38081041+ScottThomas07@users.noreply.github.com>
2018-04-11 13:46:18 -0400

    Add files via upload

--- face_rec.py
+++ face_rec.py
@@ -139,17 +139,18 @@ def predict(X_img, knn_clf=None, model_path=None, distance_threshold=0.6):
 
     X_face_locations = face_recognition.face_locations(X_img)
 
-    # If no faces are found in the image, return an empty result.
+# If no faces are found in the image, return an empty result.
     if len(X_face_locations) == 0:
+        print("\n**debug: no faces found**\n")
         return []
 
-    # Find encodings for faces in the test iamge
+# Find encodings for faces in the test iamge
     faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
 
     # Use the KNN model to find the best matches for the test face
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
     are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
-
+    
     # Predict classes and remove classifications that aren't within the threshold
     return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
 
