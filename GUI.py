c13f567203d9a884de05237b743f9c0393bf5900
mrosenf2 <matthew.rosenfeld22@gmail.com>
2018-05-02 15:32:01 -0400

    added client.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,4 @@
-import time
 import tkinter
-import face_rec2
 from threading import Thread
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
@@ -9,40 +7,12 @@ import cv2
 import os
 import socket
 import pickle
+import threading
 
 # START GUI DEFINITIONS
 # main window
 top = tkinter.Tk()
 top.title("Team Shaspasms")
-
-# setup client connection
-# TCP_IP = '10.104.190.43'
-TCP_IP = '127.0.0.1'
-host, _, _ = socket.gethostbyaddr(TCP_IP)
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-# set up video
-
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-
-def sendFrame(frm):
-    # create frame as bytes
-    frm = pickle.dumps(frm)
-    # send frame length
-    s.sendall(str(len(frm)).encode())
-    # wait for confirmation of frame lenght
-    recv = s.recv(BUFFER_SIZE).decode()
-    # send frome
-    s.sendall(frm)
-    # wait for confirmation of frame
-    recv = s.recv(BUFFER_SIZE).decode()
-    
-def closeconn():
-    s.close()
-
 
 
 
@@ -60,22 +30,6 @@ def button_Go():
     # vidPath = txt_fn.get("1.0", "end-0c")
     # tracker = detect_trackClass.faceTracker(vidPath)
     capture = cv2.VideoCapture(vidPath)
-    try:
-        while True:
-            ret, frame = capture.read()
-            if not ret:
-                print('end of video')
-                break
-
-            try:
-                sendFrame(frame)
-            except (ConnectionAbortedError, ConnectionResetError) as e:
-                print("Cxn was terminated")
-                top.destroy()
-                break
-    except KeyboardInterrupt as e:
-        top.destroy()
-        pass
 
 
 # widgets
